5cad1df1a1b8986498d4e8b9312d4dcb45b2dbad
Deeha823 <wadeehamj@gmail.com>
2022-03-21 18:46:10 -0400

    Updating

--- 01-Lesson-Plans/02-Python/1/Activities/03-Stu_Jupyter_Project/Unsolved/hello_world.py
+++ 01-Lesson-Plans/02-Python/1/Activities/03-Stu_Jupyter_Project/Unsolved/hello_world.py
@@ -2,3 +2,4 @@
 print("hello world")
 
 # print a statement: 'Hello World from a new cell'
+print('Hellow world from a new cell')
---
+++ 01-Lesson-Plans/02-Python/1/Activities/07-Stu_Conditionals/Unsolved/Sam_test_conditions.py
@@ -0,0 +1,29 @@
+# 1. 
+x = 5
+y = 10
+if 2 * x > 10:
+    print("Question one work!")
+else:
+    print("Oooo needs some work")
+
+#2.
+x = 5
+y = 10
+if len("Dog") < x:
+    print("Question 2 works!")
+else:
+    print("Still missing out")
+
+#3.
+age = 21
+if age > 20:
+    print("You are of drinking age!")
+else:
+    print("Arggggh! You think you can hoodwink me, matey You're too young to dirnk")
+
+# 4.
+x = 2
+y = 5
+if (x ** 3 >=y) and (y ** 2 < 26):
+    print("Got question 4!")
+
